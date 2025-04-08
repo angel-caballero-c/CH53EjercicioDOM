498b77e464e4edcc5398da631b65001fb3463380
Angel Caballero <caballerocedillo.la@gmail.com>
2025-04-08 13:13:02 -0600

    cambios nuevos a main.js

--- Js/main.js
+++ Js/main.js
@@ -1,32 +1,61 @@
-let btnMostrar = document.getElementById("btnMostrar");
 let encabezado1 = document.getElementById("encabezado1");
 let encabezado2 = document.getElementById("encabezado2");
-
-let lista = document.getElementsByTagName("ul");
+let listas = document.getElementsByTagName("ul");
 let elementos = document.getElementsByClassName("list-group-item");
+
 let otroElemento = document.querySelector("ul>li");
-let otrosElementos = document.querySelectorAll("ul>li");
 
-console.log(otrosElementos.length);
-console.log("otro elemento", otroElemento);
-console.log(lista.length);
-console.log(lista.item(1));
-console.log(elementos.item(2));
+let otrosElemento = document.querySelectorAll("ul>li");
+
+let btnMostrar = document.getElementById("btnMostrar");
+
+console.log(listas.length);
+console.log(listas.item(1));
+
 console.log(elementos.length);
+console.log(elementos.item(2));
 
-encabezado1.innerText = "Ejercicio DOM"
-encabezado1.innerHTML = "<em>Ejercicio</em> DOM";
-encabezado2.innerText = "DOM Exercise";
+console.log("otroElemento", otroElemento);
+console.log(otrosElemento.length);
+
+function modifica() {
+    encabezado1.innerText = "Ejercicio DOM";
+    console.log(encabezado1.innerText);
+
+    encabezado2.innerHTML = "DOM <em>Exercise</em>";
+    console.log(encabezado2.innerText);
+}
 
 btnMostrar.addEventListener("click", function(event){
-  event.preventDefault();
-  let element = document.createElement("li");
-  element.innerText="Another item";
-  element.classList.add("list-group-item");
-  
-  let element2 = element.cloneNode(true);
-  lista.item(0).before(element);
-  lista.item(0).prepend(element2);
-  lista.item(0).append(element);
-  lista.item(0).after(element2);
+    event.preventDefault();
+    console.log("Botón btnMostrar presionado");
+    let element = document.createElement("li");
+    element.innerText = "Otro item";
+    element.classList.add("list-group-item");
+
+    // let element2 = element.cloneNode(true);
+    // let element3 = element.cloneNode(true);
+    // let element4 = element.cloneNode(true);
+
+    // listas.item(0).before(element);
+    // listas.item(0).prepend(element2);
+    // listas.item(0).append(element3);
+    // listas.item(0).after(element4);
+
+    // let element5 = element.cloneNode(true);
+    // let element6 = element.cloneNode(true);
+    // listas.item(1).insertAdjacentElement("afterbegin", element5);
+    // listas.item(1).insertAdjacentElement("beforeend", element6);
+
+    listas.item(1).insertAdjacentHTML("beforebegin",
+        `<li class="list-group-item">beforebegin item</li>`);
+
+    listas.item(1).insertAdjacentHTML("afterbegin",
+        `<li class="list-group-item">afterbegin item</li>`);
+
+    listas.item(1).insertAdjacentHTML("beforeend",
+        `<li class="list-group-item">beforeend item</li>`);
+
+    listas.item(1).insertAdjacentHTML("afterend",
+        `<li class="list-group-item">afterend item</li>`);
 });
